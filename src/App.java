5afeabbafceba68640214707506cb87594921274
GabeSankar <gabesankar1@gmail.com>
2021-02-15 19:28:31 -0500

    Loss function added

--- src/App.java
+++ src/App.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class App{
 
+	//Important this is the itteration/epoch
+	public int GlobalItteration = 0;
+
 	public LinkedList<Vector<Double>> bufferhidden = new LinkedList<Vector<Double>>();
 	public LinkedList<Vector<Double>> bufferout = new LinkedList<Vector<Double>>();
 	public LinkedList<Vector<Double>> trainvec = new LinkedList<Vector<Double>>();
@@ -11,7 +14,6 @@ public class App{
 	public static void main(String[] args) {
 		
 	}
-	//write code for the sigmoid function
 	public Vector<Double> sigmoid(Vector<Double> x){
 		
 		Vector<Double> endvec = new Vector<Double>(x.size());
@@ -98,6 +100,33 @@ public class App{
 		Vector<Double> h1notactivated = vectoraddition(vecmult(hwvec, hprev),vecmult(xwvec, xin));
 
 		return vecmult(ywvec, sigmoid(h1notactivated));
+
+	}
+	public double dotproduct(Vector<Double> vect1,Vector<Double> vect2){
+
+		double finalvectordot = 0;
+
+		if(vect1.size() == vect2.size()){
+
+			for(int d = 0; d < vect1.size(); d++){
+
+			finalvectordot = finalvectordot + (vect1.get(d)*vect2.get(d));
+
+			}
+
+		}else{
+
+			System.out.println("error vector size not equal");
+
+		}
+
+		return finalvectordot;
+
+	}
+	public double Loss(LinkedList<Vector<Double>> out ,int itteration,LinkedList<Vector<Double>> trainbuffer){
+
+		return dotproduct(vectoraddition(out.get(itteration), trainbuffer.get(itteration))
+						 ,vectoraddition(out.get(itteration), trainbuffer.get(itteration)));
 
 	}
 
