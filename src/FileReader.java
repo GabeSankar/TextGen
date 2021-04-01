aa9625f36382ac9ad8aa2b35f131405412a3fcac
GabeSankar <gabesankar1@gmail.com>
2021-04-01 13:34:38 -0400

    File reader for training data set finished.

--- src/FileReader.java
+++ src/FileReader.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Map.Entry;
 
 public class FileReader {
     
@@ -64,16 +65,33 @@ public class FileReader {
 		return vec;
 
 	}
-
-    //Take care of the file reading later.
-
     public LinkedList<Vector<Double>> Vectorencoderfinal(String file) throws IOException{
 
         encodingKeyCreate(file);
         LinkedList<Vector<Double>> returnvectorlist = new LinkedList<Vector<Double>>();
+        File textfile = new File(file);
+        Scanner in = new Scanner(textfile);
+
+        while(in.hasNextByte()){
+
+            getKeysByValue(encodingKey, (char)in.nextByte());
+
+        }
+        in.close();
 
         return returnvectorlist;
 
     }
+    public static <T, E> Set<T> getKeysByValue(HashMap<T, E> map, E value) {
+        Set<T> keys = new HashSet<T>();
+        for (Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
+        }
+        return keys;
+    }
+    
+    
 
 }
