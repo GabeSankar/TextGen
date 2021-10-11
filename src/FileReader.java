84bc7e22e17e18a1c6314decaff1a3dd40893f99
GabeSankar <gabesankar1@gmail.com>
2021-10-11 13:34:53 -0400

    comments

--- src/FileReader.java
+++ src/FileReader.java
@@ -6,8 +6,10 @@ import java.util.Map.Entry;
 
 public class FileReader {
     
+    //Hash map with one hot vector list of keys with each vector being the key to the represented character.
     public HashMap<Vector<Double>,Character> encodingKey = new HashMap<Vector<Double>,Character>();
 
+    //Method that returns a list of all the characters in the train/test file.
     private ArrayList<Character> charCounter(String filename) throws IOException{
 
         ArrayList<Character> allcharsinfile = new ArrayList<Character>();
@@ -37,6 +39,7 @@ public class FileReader {
         return allcharsinfile;
 
     }
+    //This method takes character list and removes the repeating characters.
     private void encodingKeyCreate(String file)throws IOException{
 
         ArrayList<Character> baseArraylist = charCounter(file);
@@ -51,6 +54,7 @@ public class FileReader {
 
 
     }
+    //Depending on the amount of unique characters a unnique one hot vector is returned.
     private Vector<Double> nullvectormaker(int size,int itter){
 
 		Vector<Double> vec = new Vector<Double>(size);
@@ -65,6 +69,7 @@ public class FileReader {
 		return vec;
 
 	}
+    //Returns the final list of the characters paired to the one hot keys.
     public LinkedList<Vector<Double>> Vectorencoderfinal(String file) throws IOException{
 
         encodingKeyCreate(file);
@@ -82,6 +87,7 @@ public class FileReader {
         return returnvectorlist;
 
     }
+    //Method is for getting the one hot key from the value.
     public static <T, E> Set<T> getKeysByValue(HashMap<T, E> map, E value) {
         Set<T> keys = new HashSet<T>();
         for (Entry<T, E> entry : map.entrySet()) {
--- src/Gradient.java
+++ src/Gradient.java
@@ -3,14 +3,17 @@ import java.util.*;
 
 public class Gradient {
 
+    //Instance of VectorMath class.
     VectorMath vec = new VectorMath();
  
+    //Derivative of sigmoid function.
     private double sigmoiderivative(double input){
 
         double sigmoidDEnding = (1 - vec.scalarsigmoid(input));
         return vec.scalarsigmoid(input)*sigmoidDEnding;
 
     }
+    //From line 13 to 59 there would be calculations for all or the relevant partial derivatives of the forwad propagation algorithm.
     private double partialSummationDerivativeH(double hprev){
 
         return hprev;
@@ -38,6 +41,7 @@ public class Gradient {
         sigmoiderivative(h1notactivated)*partialDerivativeYSig(hlayeractivated)*partialLY(CorrectVec, index);
 
     }
+
     public double finalpartialDerivativeH(Vector<Double> invec,Vector<Double> xweight,int index,double h1notactivated,
                                          double hlayeractivated,Vector<Double> CorrectVec){
 
